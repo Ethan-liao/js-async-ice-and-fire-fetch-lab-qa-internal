b6be42fb10eb37ffaee42464945a1802e9a9289d
Ethan-liao <ethan-liao@outlook.com>
2018-09-25 22:08:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ function fetchBooks() {
   //write fetch request to the Game of Thrones API
    fetch("https://anapioficeandfire.com/api/books")
     .then(resp => resp.json())
-    .then(json => renderBooks(json))
+    .then(json => renderBooks(json) + console.log(json))
+    
 }
 
 function renderBooks(json) {
+  let counter = 0;
   const main = document.querySelector('main')
   json.forEach(book => {
+
+// start new expression    
+    if(counter === 4){
+    const fifthBook = document.createElement('h1');
+    fifthBook.innerHTML = `This ${book.name} is the 5th book`
+    main.appendChild(fifthBook)
+    }
+// end of new expresssion     
+
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
+    counter++
   })
+  
+  
 }
 
 document.addEventListener('DOMContentLoaded', function() {
